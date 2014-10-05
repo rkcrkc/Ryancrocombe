4673dde4d352889f0ebb75de9f61d6f3686a7116
Ryan Crocombe <ryancrocombe@gmail.com>
2014-10-05 10:52:08 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token
+		File.read(token_file).chomp
+	else
+		# Generate a new token and store it in token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+Ryancrocombe::Application.config.secret_key_base = '2ab26f635da1b568f39fcefaad7cb6a50fe8de260cd670baa99f9258b22a317ef96c5c4806de38bb59907f69454f4f34924c11abc655c0dd627e6279c0059443'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ryancrocombe::Application.config.session_store :cookie_store, key: '_ryancrocombe_session'
