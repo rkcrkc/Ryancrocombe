80bce136ddafb326e9595ba601ced5b9cc51dea2
RyanCrocombe <ryancrocombe@gmail.com>
2016-06-21 16:20:19 +0800

    Add English editing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ryancrocombe::Application.routes.draw do
   root "pages#home"
+  get "english" => "pages#english"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
